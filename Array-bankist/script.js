5c1ecc8cfc9af7e4ed107d7606f2d81c9c543c57
Meet-510 <mistrymeet941@gmail.com>
2025-10-15 10:11:25 -0600

    updating bankist

--- Array-bankist/script.js
+++ Array-bankist/script.js
@@ -1,64 +1,3 @@
-// let arr = [0, 1, 2, 3, 4, 5, 6];
-// // reverse method actually mutate the original array
-// console.log(arr.reverse());
-// // slice do not mutate the original array
-// console.log(arr.slice(-1));
-// console.log([...arr]);
-// console.log(arr.slice());
-
-// // splice actually mutate the original array
-// console.log(arr.splice(3, 3));
-// console.log(arr);
-
-// // concat
-// let arr2 = [66, 77, 888, 999, 9999];
-// let concatArr = arr.concat(arr2);
-// console.log(concatArr);
-// console.log([...arr, ...arr2]);
-
-// // join
-// console.log(concatArr.join(" № "));
-
-// let movement = [100, 200, -300, 3000, 1000, -29292, 200000];
-// for (let [i, move] of movement.entries()) {
-//   if (move > 0) {
-//     console.log(` YOu diposted  ${move}`);
-//   } else {
-//     console.log(`YOu withdrew   ${Math.abs(move)}`);
-//   }
-// }
-// console.log("-----------------------");
-// // forEach loop movement.forEach(function (current element, index , array))
-
-// movement.forEach(function (move, index, arr) {
-//   if (move > 0) {
-//     console.log(`YOu diposted ${index + 1} - ${move}`);
-//   } else {
-//     console.log(`YOu withdrew   ${Math.abs(move)}`);
-//   }
-// });
-
-// let students = new Map([
-//   [1, "Meet Mistry"],
-//   [2, "Madhav"],
-//   [3, "Nisarg"],
-//   [4, "Anand "],
-// ]);
-
-// students.forEach(function (val, ind, arr) {
-//   console.log(`Er ${ind} is for ${val}`);
-//   console.log("here is entire array", ...arr);
-// });
-
-// let newSet = new Set(["USD", "CAD", "USD", "CAD", "IND", "JPD"]);
-// newSet.forEach(function (value, key) {
-//   console.log(`${key} ${value}`);
-// });
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
 // Data
 const account1 = {
   owner: "Jonas Schmedtmann",
@@ -116,156 +55,54 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-// ?moments//////////////////////////////////////////
-let displayMovements = function (movements) {
+const displayMovements = function (movements) {
   containerMovements.innerHTML = " ";
-  ``;
-  // .textContent = 0
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
     const html = `
-    <div class="movements__row">
-    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-   
-    <div class="movements__value">${mov}💲</div>
-</div>
+         <div class="movements__row">
+         <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+
+         <div class="movements__value">${mov}💲</div>
+         </div>
     `;
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
+displayMovements(account1.movements);
 
-// /////////////////////////////////////////////////////////
-const calcDisplayBalance = function (acc) {
-  acc.balance = acc.movements.reduce(function (acc, cur, i, arr) {
-    // console.log(`Iteration no ${i} : ${acc}`);
-    return acc + cur;
-  }, 0);
-
-  labelBalance.textContent = `${acc.balance}💲`;
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} ERU`;
 };
+calcDisplayBalance(account1.movements);
 
-// display summary///////////////////////////////////////
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}`;
 
-const calcDisplaySummary = function (acc) {
-  const income = acc.movements
-    .filter(function (mov) {
-      return mov > 0;
-    })
-    .reduce(function (acc, mov) {
-      return (acc += mov);
-    });
-  labelSumIn.textContent = `${income}💲`;
+  const outs = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outs)}`;
 
-  const outcome = acc.movements
-    .filter(function (mov) {
-      return mov < 0;
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposite) => (deposite * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
     })
-    .reduce(function (acc, mov) {
-      return (acc += mov);
-    }, 0);
-  labelSumOut.textContent = `${Math.abs(outcome)}💲`;
+    .reduce((acc, int) => acc + int, 0);
 
-  const interest = acc.movements
-    .filter(function (mov) {
-      return mov > 0;
-    })
-    .map(function (m) {
-      return (m * acc.interestRate) / 100;
-    })
-    .filter(function (mov) {
-      return mov >= 1;
-    })
-    .reduce(function (acc, mov) {
-      return (acc += mov);
-    }, 0);
-  labelSumInterest.textContent = `${interest}💲`;
+  labelSumInterest.textContent = `${interest}`;
 };
-const updateUI = function (acc) {
-  // display movements
-  displayMovements(acc.movements);
-  // display balance
-  calcDisplayBalance(acc);
-  // display summary
-  calcDisplaySummary(acc);
-};
-// event handlers
-let currentAccount;
-btnLogin.addEventListener("click", function (e) {
-  e.preventDefault();
+calcDisplaySummary(account1.movements);
 
-  currentAccount = accounts.find(function (acc) {
-    return acc.username === inputLoginUsername.value;
-  });
-  console.log(currentAccount);
-
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    // display ui and welcome msg
-    labelWelcome.textContent = `Welcome back ${
-      currentAccount.owner.split(" ")[0]
-    }`;
-    containerApp.style.opacity = 100;
-    // clear the login details
-    inputLoginUsername.value = inputLoginPin.value = " ";
-    inputLoginPin.blur();
-    updateUI(currentAccount);
-  }
-});
-
-btnTransfer.addEventListener("click", function (e) {
-  e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
-  const receiverAcc = accounts.find(
-    (acc) => acc.username === inputTransferTo.value
-  );
-  inputTransferAmount.value = inputTransferTo.value = "";
-
-  if (
-    amount > 0 &&
-    receiverAcc &&
-    currentAccount.balance >= amount &&
-    receiverAcc?.username !== currentAccount.username
-  ) {
-    // Doing the transfer
-    currentAccount.movements.push(-amount);
-    receiverAcc.movements.push(amount);
-
-    // Update UI
-    updateUI(currentAccount);
-  }
-});
-// //////////////////////////////////////
-// map mathod is like forEach method but its return new Array
-let movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-// lets convert this euros into us dollar
-const eurToUsd = 1.1;
-const momentsToUsd = movements.map(function (mov) {
-  return Math.trunc(mov * eurToUsd);
-  //   return mov;
-});
-console.log(momentsToUsd);
-console.log(movements);
-
-// now lets make new same array with the help of for of loop
-
-// let newArry = [];
-// for (let mov of movements) {
-//   newArry.push(mov * eurToUsd);
-// }
-// console.log(newArry);
-
-let movementsDescription = movements.map(function (mov, i, arr) {
-  if (mov > 0) {
-    return `Movement ${i + 1}: You deposited ${Math.abs(mov)}`;
-  } else {
-    return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
-  }
-});
-console.log(movementsDescription);
-
-///////////////////////////////////////////////////////////////////////////
-// computing usernames
-function createUserName(accs) {
+const createUserNames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
       .toLowerCase()
@@ -275,53 +112,5 @@ function createUserName(accs) {
       })
       .join("");
   });
-}
-createUserName(accounts);
-console.log(accounts);
-// console.log(createUserName("Meet Vijaybhai Mistry"));
-// jyare pn tme return karta hov to call karela function ne tamare log karavo pdse
-
-const deposits = movements.filter(function (mov) {
-  return mov > 0;
-});
-console.log(deposits);
-// console.log(movements);
-
-const withdrawals = movements.filter(function (mov) {
-  return mov < 0;
-});
-console.log(withdrawals);
-////////////////////////////////////////////
-
-const balance = movements.reduce(function (acc, cur, i, arr) {
-  console.log(`Iteration no ${i} : ${acc}`);
-  return acc + cur;
-}, 0);
-console.log(balance);
-
-// Find maximum in moments array by reduce method
-const max = movements.reduce(function (acc, mov) {
-  if (acc > mov) {
-    return acc;
-  } else {
-    return mov;
-  }
-}, movements[0]);
-console.log(max);
-
-// for closing the account
-btnClose.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (
-    inputCloseUsername.value === currentAccount.username &&
-    inputClosePin.value === currentAccount.pin
-  ) {
-    const index = accounts.findIndex(function (acc) {
-      acc.username === currentAccount.username;
-    });
-    // console.log(index);
-    accounts.splice(index, 1);
-    // hide ui after deleting user
-    containerApp.style.opacity = 0;
-  }
-});
+};
+createUserNames(accounts);
--- Basics/script.js
+++ Basics/script.js
@@ -1,93 +1,176 @@
-const transactionsEl = document.querySelector(".transactions");
-const balanceNumberEl = document.querySelector(".balance-number");
-const numberIncomeEl = document.querySelector(".number--income");
-const numberExpensesEl = document.querySelector(".number--expenses");
-const formEl = document.querySelector(".form");
-const inputDescriptionEl = document.querySelector(".input--description");
-const inputAmountEl = document.querySelector(".input--amount");
+"use strict";
+// const transactionsEl = document.querySelector(".transactions");
+// const balanceNumberEl = document.querySelector(".balance-number");
+// const numberIncomeEl = document.querySelector(".number--income");
+// const numberExpensesEl = document.querySelector(".number--expenses");
+// const formEl = document.querySelector(".form");
+// const inputDescriptionEl = document.querySelector(".input--description");
+// const inputAmountEl = document.querySelector(".input--amount");
 
-formEl.addEventListener("submit", function (event) {
-  // prevent default browser
-  event.preventDefault();
+// formEl.addEventListener("submit", function (event) {
+//   // prevent default browser
+//   event.preventDefault();
 
-  // get input values
-  const description = inputDescriptionEl.value;
-  const amount = +inputAmountEl.value;
+//   // get input values
+//   const description = inputDescriptionEl.value;
+//   const amount = +inputAmountEl.value;
 
-  // create html
-  const transactionItemHTML = `
-        <li class="transaction transaction--${
-          amount > 0 ? "income" : "expense"
-        }">
-          <span class="transaction__text">${description}</span>
-          <span class="transaction__amount">${
-            amount > 0 ? "+" : ""
-          }${amount}</span>
-          <button class="transaction__btn">X</button>
-        </li>
-       
-  `;
+//   // create html
+//   const transactionItemHTML = `
+//         <li class="transaction transaction--${
+//           amount > 0 ? "income" : "expense"
+//         }">
+//           <span class="transaction__text">${description}</span>
+//           <span class="transaction__amount">${
+//             amount > 0 ? "+" : ""
+//           }${amount}</span>
+//           <button class="transaction__btn">X</button>
+//         </li>
 
-  // insert HTML to page
-  transactionsEl.insertAdjacentHTML("beforeend", transactionItemHTML);
+//   `;
 
-  //celar form inputs
-  inputDescriptionEl.value = "";
-  inputAmountEl.value = "";
+//   // insert HTML to page
+//   transactionsEl.insertAdjacentHTML("beforeend", transactionItemHTML);
 
-  // unfocus input form
-  inputAmountEl.blur();
-  inputDescriptionEl.blur();
+//   //celar form inputs
+//   inputDescriptionEl.value = "";
+//   inputAmountEl.value = "";
 
-  // update the income or expense
-  if (amount > 0) {
-    const currentIncome = +numberIncomeEl.textContent;
-    const updatedIncome = currentIncome + amount;
-    numberIncomeEl.textContent = updatedIncome;
-  } else {
-    const currentExpenses = +numberExpensesEl.textContent;
-    const updatedExpenses = currentExpenses + amount * -1;
-    numberExpensesEl.textContent = updatedExpenses;
+//   // unfocus input form
+//   inputAmountEl.blur();
+//   inputDescriptionEl.blur();
+
+//   // update the income or expense
+//   if (amount > 0) {
+//     const currentIncome = +numberIncomeEl.textContent;
+//     const updatedIncome = currentIncome + amount;
+//     numberIncomeEl.textContent = updatedIncome;
+//   } else {
+//     const currentExpenses = +numberExpensesEl.textContent;
+//     const updatedExpenses = currentExpenses + amount * -1;
+//     numberExpensesEl.textContent = updatedExpenses;
+//   }
+
+//   // update balance
+//   const income = +numberIncomeEl.textContent;
+//   const expense = +numberExpensesEl.textContent;
+//   const updatedBalance = income - expense;
+//   balanceNumberEl.textContent = updatedBalance;
+
+//   // check if negative balance and make it red
+//   if (income - expense < 0) {
+//     balanceNumberEl.style.color = "red";
+//   }
+// });
+
+// function clickHandler(event) {
+//   // remove transaction item visually
+//   const clickedEl = event.target.parentNode;
+//   clickedEl.remove();
+//   // update the total after removing the item
+//   const amountEl = clickedEl.querySelector(".transaction__amount");
+//   const amount = +amountEl.textContent;
+//   if (amount > 0) {
+//     const currentIncome = +numberIncomeEl.textContent;
+//     const updatedIncome = currentIncome - amount;
+//     numberIncomeEl.textContent = updatedIncome;
+//   } else {
+//     const currentExpenses = +numberExpensesEl.textContent;
+//     const updatedExpenses = currentExpenses - amount * -1;
+//     numberExpensesEl.textContent = updatedExpenses;
+//   }
+
+//   // update the main balance
+//   const income = +numberIncomeEl.textContent;
+//   const expense = +numberExpensesEl.textContent;
+//   balanceNumberEl.textContent = income - expense;
+
+//   // check if negative balance and make it red
+//   if (income - expense < 0) {
+//     balanceNumberEl.style.color = "red";
+//   }
+// }
+
+// transactionsEl.addEventListener("click", clickHandler);
+
+// this keyword
+
+// const birthyear = 2002;
+// const salary = 200;
+// const meet = {
+//   job: "superstore",
+//   salary: 15,
+//   birthyear: 2000,
+//   calcAge: function () {
+//     console.log(2025 - birthyear);
+//   },
+//   greetArr: () => {
+//     console.log(this.salary);
+//   },
+//   greetMethod: function () {
+//     console.log(this.salary);
+//   },
+// };
+// meet.calcAge();
+// meet.greetArr();
+// meet.greetMethod();
+
+// array destructuring
+
+let Meet = 5000;
+let Manan = 10000;
+[Meet, Manan] = [Manan, Meet];
+console.log(Meet);
+
+const restaurant = {
+  name: "Punjab da tadka",
+  location: "Dholka",
+  categories: ["veg", "Non-veg", "Italian", "Pizzethisria"],
+  starter: ["Manchurian", "Harabhara kabab", "Soup", "Pizza"],
+  mainMenu: ["Dalbhat", "shak", "rotli", "mohanthal"],
+  openingHours: {
+    mon: {
+      open: 10,
+      close: 22,
+    },
+    tue: {
+      open: 10,
+      close: 22,
+    },
+    wed: {
+      open: 10,
+      close: 22,
+    },
+  },
+
+  order: function (starterInd, mainInd) {
+    return [this.starter[starterInd], this.mainMenu[mainInd]];
+  },
+};
+// console.log(restaurant.order(2, 1));
+const { name: naam, location: pata, mainMenu: shuchekhavama } = restaurant;
+console.log(naam, pata, shuchekhavama);
+const { wed: budhvaar } = restaurant.openingHours;
+console.log(budhvaar);
+
+// the spread operator
+const arr = [1, 2, 3, 4];
+const newArr = [99, 22, 33, ...arr];
+console.log(newArr);
+
+// The REST patterns
+// the opposite of spread operator
+// it will be on left side of assignment operator
+// the rest patterns takes multiple values and packs them into one Array
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
   }
-
-  // update balance
-  const income = +numberIncomeEl.textContent;
-  const expense = +numberExpensesEl.textContent;
-  const updatedBalance = income - expense;
-  balanceNumberEl.textContent = updatedBalance;
-
-  // check if negative balance and make it red
-  if (income - expense < 0) {
-    balanceNumberEl.style.color = "red";
-  }
-});
-
-function clickHandler(event) {
-  // remove transaction item visually
-  const clickedEl = event.target.parentNode;
-  clickedEl.remove();
-  // update the total after removing the item
-  const amountEl = clickedEl.querySelector(".transaction__amount");
-  const amount = +amountEl.textContent;
-  if (amount > 0) {
-    const currentIncome = +numberIncomeEl.textContent;
-    const updatedIncome = currentIncome - amount;
-    numberIncomeEl.textContent = updatedIncome;
-  } else {
-    const currentExpenses = +numberExpensesEl.textContent;
-    const updatedExpenses = currentExpenses - amount * -1;
-    numberExpensesEl.textContent = updatedExpenses;
-  }
-
-  // update the main balance
-  const income = +numberIncomeEl.textContent;
-  const expense = +numberExpensesEl.textContent;
-  balanceNumberEl.textContent = income - expense;
-
-  // check if negative balance and make it red
-  if (income - expense < 0) {
-    balanceNumberEl.style.color = "red";
-  }
-}
-
-transactionsEl.addEventListener("click", clickHandler);
+  console.log(sum);
+};
+const x = [10, 20, 10];
+add(...x);
+add(1, 2, 3, 4, 5);
+add(2, 3, 1);
